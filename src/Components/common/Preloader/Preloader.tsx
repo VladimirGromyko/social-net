ec9924c6a90b55f5b02161ff01e3f602e7216fef
VladimirGromyko <vl-mailbox@mail.ru>
2021-10-01 15:00:52 +0300

    "10-09-2021 commit - with follow-unfollow"

--- src/Components/common/Preloader/Preloader.tsx
+++
@@ -1,8 +0,0 @@
-import preloader from "../../../assets/images/1495 (1).gif";
-
-let Preloader = () => {
-    return <div style={{backgroundColor: 'white'}}>
-        <img src={preloader}/>
-    </div>
-}
-export default Preloader
--- src/redux/sidebar-reducer.ts
+++
@@ -1,9 +0,0 @@
-import {ActionsTypes, SidebarType} from "./store";
-
-let initialState = {}
-
-export const sidebarReducer = (state: SidebarType = initialState, action: ActionsTypes) => {
-
-    return state
-}
-export default sidebarReducer
