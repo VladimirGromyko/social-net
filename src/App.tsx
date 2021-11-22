539a0205393ae8434926aeffd0545c9347361dc9
VladimirGromyko <vl-mailbox@mail.ru>
2021-11-22 14:55:14 +0300

    '22.11.2021 - with Lazy and Suspense'

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React, {ComponentType} from 'react';
+import React, {ComponentType, lazy, Suspense} from 'react';
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
 import {HashRouter, Route} from 'react-router-dom';
 import News from "./Components/News/News";
 import Music from "./Components/Music/Music";
 import Settings from "./Components/Settings/Settings";
-import DialogsContainer from "./Components/Dialogs/DialogsContainer";
+
 import UsersContainer from "./Components/Users/UsersContainer";
-import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderContainer from "./Components/Header/headerContainer";
 import LoginPage from "./Components/Login/Login";
 import {connect, Provider} from "react-redux";
@@ -16,6 +15,11 @@ import {compose} from "redux";
 import store, {AppStoreType} from "./redux/redux-store";
 import Preloader from "./Components/common/Preloader/Preloader";
 import {initializeApp} from "./redux/app-reducer";
+import {withSuspense} from "./hoc/withSuspense";
+
+
+const DialogsContainer = lazy(() => import('./Components/Dialogs/DialogsContainer'));
+const ProfileContainer = lazy(() => import('./Components/Profile/ProfileContainer'));
 
 type AppPropsType = {
     initializeApp: () => void
@@ -36,16 +40,13 @@ class App extends React.Component <AppPropsType> {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-
-                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                    <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer/>}/>
+                    <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+                    <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
                     <Route path='/users' render={() => <UsersContainer/>}/>
                     <Route path='/news' render={() => <News/>}/>
                     <Route path='/music' render={() => <Music/>}/>
                     <Route path='/settings' render={() => <Settings/>}/>
                     <Route path='/login' render={() => <LoginPage/>}/>
-
                 </div>
             </div>
         )
--- src/Components/Dialogs/DialogsContainer.tsx
+++ src/Components/Dialogs/DialogsContainer.tsx
@@ -1,74 +1,26 @@
 import React from "react";
-import {ActionsTypes, RootStateType, SendMessageType, SubscribeType} from "../../redux/store";
+import {ActionsTypes} from "../../redux/store";
 import {sendMessageAC} from "../../redux/dialogs-reducer";
-import Dialogs, {DialogsAndMessagesType} from "./Dialogs";
-import { connect } from "react-redux";
-import {UserDataType} from "../../redux/auth-reducer";
+import Dialogs from "./Dialogs";
+import {connect} from "react-redux";
+
 import {AppStoreType} from "../../redux/redux-store";
 import {Redirect} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
 
-
-type StoreType = {
-    _state: RootStateType
-    _callSubscriber: () => void
-    getState: () => RootStateType
-    subscribe: SubscribeType
-    dispatch: (action: ActionsTypes) => void
-}
-type StoreOfType = {
-    store: StoreType
-}
-
-// const DialogsContainer: React.FC<StoreOfType> = (props) => {
-//
-//     let state = props.store.getState().dialogsPage
-//     let onSendMessageClick = () => {
-//         props.store.dispatch(sendMessageAC())
-//     }
-//     let onNewMessageChange = (body:string) => {
-//         let action = updateNewMessageBodyAC(body)
-//         props.store.dispatch(action)
-//     }
-//     return (
-//        <Dialogs updateNewMessageBody ={onNewMessageChange}
-//                 sendMessage = {onSendMessageClick}
-//                 dialogsPage = {state} />
-//     )
-// }
-
-let mapStateToProps =(state: AppStoreType) => {
+let mapStateToProps = (state: AppStoreType) => {
     return {
-        dialogsPage: state.dialogsPage,
-        //isAuth: state.auth.isAuth
+        dialogsPage: state.dialogsPage
     }
 }
-let mapDispatchToProps =(dispatch: (action: ActionsTypes) => void) => {
+let mapDispatchToProps = (dispatch: (action: ActionsTypes) => void) => {
     return {
-        // updateNewMessageBody: (body:string) => {
-        //     let action = updateNewMessageBodyAC(body)
-        //     dispatch(action)
-        //
-        // },
-        sendMessage: (newMessageBody: string)=> {
+        sendMessage: (newMessageBody: string) => {
             dispatch(sendMessageAC(newMessageBody))
         }
     }
 }
-
-
-// let AuthRedirectComponent = withAuthRedirect(Dialogs)
-// // let AuthRedirectComponent = (props: DialogsAndMessagesType) => {
-// //     if (!props.isAuth) return <Redirect to= '/login' />
-// //     return <Dialogs { ... props}/>
-// // }
-//
-// const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent)
-//
-// export default DialogsContainer
-
 export default compose<React.ComponentType>(
     connect(mapStateToProps, mapDispatchToProps),
-    withAuthRedirect
-)(Dialogs)
+    withAuthRedirect)(Dialogs)
